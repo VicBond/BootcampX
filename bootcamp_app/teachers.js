7c801f824a0e9ca51e5a39596adff8c68f848539
Victor Bondarenko <nirasimha@gmail.com>
2021-11-25 00:20:46 +0000

    updated with parameterized query

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,9 +8,9 @@ const pool = new Pool({
 });
 
 
-
-
-
+const cohortName = process.argv[2];
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`];
 
 
 
@@ -20,9 +20,9 @@ FROM teachers
   JOIN assistance_requests ON teacher_id = teachers.id
   JOIN students ON student_id = students.id
   JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name = $1
 ORDER BY teacher;
-`)
+`,values)
   .then(res => {
     res.rows.forEach(row => {
       console.log(`${row.cohort}: ${row.teacher}`);
